ed03108c152f457fe47d664825ae00cf4f5bbbde
Shaun Luttin <admin@shaunluttin.com>
2017-08-08 15:57:22 -0700

    Update main.ts

--- aurelia-app/src/main.ts
+++ aurelia-app/src/main.ts
@@ -1,10 +1,14 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
+import oidcConfig from "./open-id-connect-configuration";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources');
+
+  aurelia.use    
+    .plugin("aurelia-open-id-connect", (callback) => callback(oidcConfig));
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
