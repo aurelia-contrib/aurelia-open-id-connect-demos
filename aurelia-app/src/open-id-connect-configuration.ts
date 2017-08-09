8774918bd76ba3b1dac22401a001239e142cd782
Shaun Luttin <admin@shaunluttin.com>
2017-08-08 18:58:23 -0700

    Wire up Azure Active Directory

--- aurelia-app/src/open-id-connect-configuration.ts
+++ aurelia-app/src/open-id-connect-configuration.ts
@@ -1,33 +1,39 @@
 import { OpenIdConnectConfiguration } from "aurelia-open-id-connect";
 import { UserManagerSettings, WebStorageStateStore } from "oidc-client";
 
-const urls = {
-  authority: "",
-  host: ""
-}
+// https://login.windows.net/common/discovery/keys does not allow CORS
+// so we need to configure these keys manually.
+import azureSigningKeys from "./azure-signing-keys";
+const appHost = "http://localhost:9000";
 
 export default {
-  loginRedirectModuleId: "home",
-  logoutRedirectModuleId: "home",
+  loginRedirectModuleId: "index",
+  logoutRedirectModuleId: "index",
   userManagerSettings: <UserManagerSettings>{
-    // number of seconds in advance of access token expiry
-    // to raise the access token expiring event
+    // The number of seconds in advance of access token expiry
+    // to raise the access token expiring event.
     accessTokenExpiringNotificationTime: 1,
-    authority: urls.authority,
-    automaticSilentRenew: false, // true,
-    // interval in milliseconds to check the user's session
+    // Your own OpenID Provider or a certified authority
+    // from the official list http://openid.net/certification/
+    // We are using a single tenant Azure Active Directory as our authority.
+    // See http://bit.ly/2wrX5Wg for details.
+    authority: "https://login.microsoft.com/0252f597-5d7e-4722-bafa-0b26f37dc14f",
+    automaticSilentRenew: false,
+    // The interval in milliseconds between checking the user's session.
     checkSessionInterval: 10000,
-    client_id: "aurelia",
+    // The client or application ID that the authority issues.
+    client_id: "dc0e8e79-e73c-4ad9-9743-b262320d77d6",
     filterProtocolClaims: true,
     loadUserInfo: false,
-    post_logout_redirect_uri: `${urls.host}/signout-oidc`,
-    redirect_uri: `${urls.host}/signin-oidc`,
-    response_type: "id_token token",
-    scope: "openid email roles profile",
+    post_logout_redirect_uri: `${appHost}/signout-oidc`,
+    redirect_uri: `${appHost}/signin-oidc`,
+    response_type: "id_token",
+    scope: "openid",
+    signingKeys: azureSigningKeys.keys,
     // number of millisecods to wait for the authorization
     // server to response to silent renew request
     silentRequestTimeout: 10000,
-    silent_redirect_uri: `${urls.host}/signin-oidc`,
+    silent_redirect_uri: `${appHost}/signin-oidc`,
     userStore: new WebStorageStateStore({
       prefix: "oidc",
       store: window.localStorage,
