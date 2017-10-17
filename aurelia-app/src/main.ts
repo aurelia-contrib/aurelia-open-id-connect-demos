7b8aa54749478925840ad5d7d4689436c3428a63
Shaun Luttin <admin@shaunluttin.com>
2017-10-16 21:45:34 -0700

    Update sample to v0.15.0

--- aurelia-app/src/main.ts
+++ aurelia-app/src/main.ts
@@ -1,14 +1,24 @@
-import {Aurelia} from 'aurelia-framework'
+import { Aurelia } from 'aurelia-framework'
+import { OpenIdConnectConfiguration } from "aurelia-open-id-connect";
 import environment from './environment';
-import oidcConfig from "./open-id-connect-configuration";
+import myOidcConfig from "./open-id-connect-configuration";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources');
 
-  aurelia.use    
-    .plugin("aurelia-open-id-connect", (callback) => callback(oidcConfig));
+  aurelia.use
+    .plugin("aurelia-open-id-connect", (oidcConfig: OpenIdConnectConfiguration) => {
+      // I am not sure that I like this. 
+      // It might be better to let the user 
+      // pass in an instance of OpenIdConnectConfiguration.
+      // The problem with using a callback, is that the user might overwrite
+      // the entire object reference (oidcConfig = myOidcConfig) and thereby break everything.
+      oidcConfig.userManagerSettings = myOidcConfig.userManagerSettings;
+      oidcConfig.loginRedirectModuleId = myOidcConfig.loginRedirectModuleId;
+      oidcConfig.logoutRedirectModuleId = myOidcConfig.logoutRedirectModuleId;
+    });
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
