73520f5aa70013229dbd626ac38fed91757928c9
Shaun Luttin <admin@shaunluttin.com>
2017-10-17 11:48:15 -0700

    Configure oidc in the new demo project

--- aurelia-app-01/src/main.ts
+++ aurelia-app-01/src/main.ts
@@ -1,10 +1,19 @@
-import {Aurelia} from 'aurelia-framework'
+import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
+import oidcConfig from "./open-id-connect-configuration";
+import { OpenIdConnectConfiguration } from "aurelia-open-id-connect";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources');
+
+  aurelia.use
+    .plugin("aurelia-open-id-connect", (config: OpenIdConnectConfiguration) => {
+      config.userManagerSettings = oidcConfig.userManagerSettings;
+      config.loginRedirectModuleId = oidcConfig.loginRedirectModuleId;
+      config.logoutRedirectModuleId = oidcConfig.logoutRedirectModuleId;
+    });
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
